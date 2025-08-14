46829bcec8794e7d10ad243e263e4d643ed344f5
dmamanipar <dmamanipar@unsa.edu.pe>
2025-08-14 11:52:05 -0500

    Ejemplo2

---
+++ POOG3/src/main/java/pe/edu/upeu/PrubasX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu;
+
+/**
+ *
+ * @author SOFTWARE
+ */
+public class PrubasX {
+    
+    public static void main(String[] args) {
+        System.out.println("ssss");
+    }
+}
