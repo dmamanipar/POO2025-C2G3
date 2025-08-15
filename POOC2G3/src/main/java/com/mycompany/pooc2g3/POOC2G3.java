aabfd5e449884f06005f675c935e3649ebe9e6c4
SacXan <isaac.ssj.0104@gmail.com>
2025-08-15 11:08:24 -0500

    primer proyecto

---
+++ POOC2G3/src/main/java/com/mycompany/pooc2g3/POOC2G3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooc2g3;
+
+/**
+ *
+ * @author SAC
+ */
+public class POOC2G3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
